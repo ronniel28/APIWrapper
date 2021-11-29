ffa53f2a84feb7e712d01b8144c7b3726b764565
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-11-29 15:18:39 +0800

    nothing

--- app/api/client.rb
+++ app/api/client.rb
@@ -1,6 +1,6 @@
 class Client
   def input_date(date)
-    connection = Faraday.new(url: "https://api.nasa.gov/planetary/apod?api_key=M3GuMI71McToxD2sDJodsKmLmCyZzlef4ciWnE2W")
+    connection = Faraday.new(url: "https://api.nasa.gov/planetary/apod?api_key=#{Rails.application.credentials.nasa[:api_key]}")
     response = connection.get("&date=#{date}")
     JSON.parse(response.body)
   end
